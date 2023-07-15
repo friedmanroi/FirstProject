9dcc8f4a381337f2f9cc19163a257a8d83d23637
friedmanroi <139368823+friedmanroi@users.noreply.github.com>
2023-07-15 10:42:55 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -1,12 +1,6 @@
 from tests.ILS import Ils
 from tests.USD import USD
-print("welcome to currency converter")
-print("please choose an option 1/2:\n 1. dollars to shekels\n 2.shekels to dollars\n")
 results = []
-def SaveList_to_File(results,results.txt):
-    with open(results.txt,'w') as file:
-        for item in results:
-            file.write(str(item) + "\n")
 def CashMachine():
     while True:
         try:
@@ -14,7 +8,7 @@ def CashMachine():
           if answer not in [1, 2]:
                raise ValueError()
 
-          if answer == 1:
+          elif answer == 1:
             value_to_convert = float(input("please enter an amount to convert\n"))
             converted_value =Ils.calculate(value_to_convert=value_to_convert)
             print(converted_value)
@@ -28,25 +22,37 @@ def CashMachine():
           break
         except ValueError:
             print("Please try again - only allowed to select 1 or 2\n")
-
-
-CashMachine()
-
+def SaveList_to_File(results,results.txt):
+    with open(results.txt,'w') as file:
+        for item in results:
+            file.write(str(item) + "\n")
 
 print("would you like to start over?\nY/N?\n")
-answer2 = input("")
-while True:
-    if 'Y' in answer2:
-        CashMachine()
-    elif 'N' in answer2:
-        print("thank you for using our currnecy convertor")
-        print(results)
-        SaveList_to_File(results,results.txt)
-
-    break
-
-
-
+answer2 = str(input())
+def SecondCycle():
+    while True:
+      try:
+        if answer2 not in ["Y","N"]:
+          raise ValueError()
+        elif 'Y' in answer2:
+         print("please choose an option 1/2:\n 1. dollars to shekels\n 2.shekels to dollars\n")
+         CashMachine()
+        elif 'N' in answer2:
+         print("thank you for using our currnecy convertor")
+         print(results)
+         SaveList_to_File(results,results.txt)
+      break
+    except ValueError:
+        print("plaese type again Y/N\n")
+def main():
+         print("welcome to currency converter")
+         print("please choose an option 1/2:\n 1. dollars to shekels\n 2.shekels to dollars\n")
+         results = []
+         CashMachine()
+         print("would you like to start over?\nY/N?\n")
+         answer2 = str(input())
+         SecondCycle()
+main()
 
 
 
